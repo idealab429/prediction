1b5c297cc3653c321b62d3dedcc73d33d2d89f15
idealab429 <idealab429@gmail.com>
2026-01-10 16:40:49 +0900

    Remove pip install command for matplotlib
    Removed installation command for matplotlib.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-pip install matplotlib
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
