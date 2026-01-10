8adb8fa23b0b01b6d7a70f334aafc5e08620a934
idealab429 <idealab429@gmail.com>
2026-01-10 16:40:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+pip install matplotlib
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
